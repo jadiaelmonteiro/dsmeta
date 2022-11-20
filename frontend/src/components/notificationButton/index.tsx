cd68bf54fced4c59473430a21f93e024d90a7fea
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2022-11-20 15:10:34 -0300

    :truck: Send notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,10 +1,24 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './style.css';
 
-function NotificationButton() {
+type Props = {
+    salesId: number;
+}
+
+function handleClick(id: number)
+{
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("mensagem enviada");
+    });
+}
+
+function NotificationButton( {salesId} : Props ) {
     return (
         <>
-            <div className="dsmeta-red-btn">
+            <div className="dsmeta-red-btn" onClick={() => handleClick(salesId)}>
                 <img src={icon} alt="Notificar" />
             </div>
         </>
