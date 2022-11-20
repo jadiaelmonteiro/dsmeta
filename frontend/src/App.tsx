3b4ea3522b0ecab3f5f1e4003d9137ec45acced7
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2022-11-20 15:14:34 -0300

    :truck: Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/header";
 import SalesCard from "./components/cardSales";
-
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './style.css';
@@ -11,7 +12,7 @@ function handleClick(id: number)
 {
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("mensagem enviada");
+        toast.info("SMS enviado com sucesso");
     });
 }
 
